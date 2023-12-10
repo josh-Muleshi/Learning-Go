cfd3c3f75d1d59204a162f7dd205a5295872f24c
joshmuleshi <jmuleshi2@gmail.com>
2023-12-10 22:27:15 +0200

    fist exercise done

--- Part1.go
+++ Part1.go
@@ -2,25 +2,83 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	getProblems, getError := os.Open("problems.csv")
+	// Define flags for the CSV file and timer
+	csvFileName := flag.String("csv", "problems.csv", "a CSV file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
 
-	if getError != nil {
-		fmt.Println("An error occurred while opening CSV file")
+	// Open the CSV file
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
+	}
+	defer file.Close()
+
+	// Parse the CSV file
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
 	}
 
-	fmt.Println("Successfully opened the CSV file")
-	defer getProblems.Close()
+	// Process the quiz
+	problems := parseLines(lines)
+	correct := 0
 
-	// read CSV file
-	fileReader := csv.NewReader(getProblems)
-	records, getError := fileReader.ReadAll()
-	if getError != nil {
-		fmt.Println(getError)
+	fmt.Println("Press Enter to start the quiz.")
+	fmt.Scanln()
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+problemLoop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.question)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's up!")
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == p.answer {
+				correct++
+			}
+		}
 	}
-	fmt.Println(records)
+
+	fmt.Printf("You got %d out of %d questions correct.\n", correct, len(problems))
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+type problem struct {
+	question string
+	answer   string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
 }
