643252bec8c527b9b510e9add8ffff07d2d4d44b
joshmuleshi <jmuleshi2@gmail.com>
2023-12-11 12:15:25 +0200

    add gorilla/mux ton my project

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Learnin-Go
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- web.go
+++ web.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Welcome to my website!")
 	})
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
 }
