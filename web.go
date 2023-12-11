d3eddea65263914e608e4b81fbe7754a955cb11b
joshmuleshi <jmuleshi2@gmail.com>
2023-12-11 12:25:46 +0200

    gorilla/mux tested

--- web.go
+++ web.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "Welcome to my website!")
+	//})
+	//
+	//fs := http.FileServer(http.Dir("static/"))
+	//http.Handle("/static/", http.StripPrefix("/static/", fs))
+	//
+	//http.ListenAndServe(":8080", nil)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
